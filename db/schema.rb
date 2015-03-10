30913345e0227d08ca0c33d5555c9863604ce856
fsp712 <fainashalts@gmail.com>
2015-03-10 15:54:05 -0700

    dropped comments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310203148) do
+ActiveRecord::Schema.define(version: 20150310225246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,14 +41,6 @@ ActiveRecord::Schema.define(version: 20150310203148) do
   create_table "carpools_users", id: false, force: :cascade do |t|
     t.integer "user_id",    null: false
     t.integer "carpool_id", null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.integer  "carpool_id"
-    t.string   "message"
-    t.string   "username"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
